c144a93a6168b590cb43d5ef182a3115c1d007bc
Konstantin Vasilev <mctep77@gmail.com>
2025-05-13 14:51:30 +0200

    add simple web server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import express from 'express';
 import { app } from './app.ts';
 import { TELEGRAM_TOKEN } from './env.ts';
 import { assert } from './util/assert.ts';
@@ -5,6 +6,17 @@ import { assert } from './util/assert.ts';
 async function main() {
 	assert(TELEGRAM_TOKEN, 'TELEGRAM_TOKEN is not defined');
 	await app(TELEGRAM_TOKEN);
+
+	const web = express();
+	const port = process.env.PORT || 4000;
+
+	web.get('/', (req, res) => {
+		res.send('I am alive!');
+	});
+
+	web.listen(port, () => {
+		console.log(`Example app listening on port ${port}`);
+	});
 }
 
 void main();
